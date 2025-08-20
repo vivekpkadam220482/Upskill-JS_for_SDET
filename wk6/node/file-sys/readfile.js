fc3c2fe97da0ccd2812bfd88358ecd7030d7995f
vivekpkadam220482 <vivek.p.kadam@gmail.com>
2025-08-20 23:53:19 +0530

    1st commit

---
+++ wk6/node/file-sys/readfile.js
@@ -0,0 +1,17 @@
+"use strict";
+
+/**
+ * File operations - Read file
+ * 
+ * @method
+ * fs.readFileSync()
+ * 
+ */
+let fs = require("fs")
+
+let filepath = `${process.cwd()}/data/sampleData.json`
+let data = fs.readFileSync(filepath, "utf-8")
+console.log(data);
+
+console.log(`I wanted to continue...`);
+
